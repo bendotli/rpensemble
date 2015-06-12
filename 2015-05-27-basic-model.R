af933fe203506f0b7984cd83277dad7903a1e336
Ben <bendotli@gmail.com>
2015-06-12 13:03:11 +0100

    Added SVM and revised parameters to match the paper.

--- 2015-05-27-basic-model.R
+++ 2015-05-27-basic-model.R
@@ -1,5 +1,6 @@
 #install.packages("RPEnsemble", type="source")
 library(RPEnsemble)
+library(caret)
 
 ## Basic model: two p-dimensional Gaussians with the same identity covariance matrix,
 ## shifted apart from each other in d of the p dimensions by 2*delta
@@ -9,16 +10,16 @@ p = 50
 d = 5
 delta = 1
 n_train = 50 # must be even
-n_test = 100 # must be even
+n_test = 1000 # must be even
 
 # Mean vectors and lists of response vectors
 mean1 = c(rep(delta, times = d), rep(0, times = p-d))
 mean2 = c(rep(-delta, times = d), rep(0, times = p-d))
 
-train.class1 = replicate(n_train/2, rnorm(p, mean1), simplify = "array")
-train.class2 = replicate(n_train/2, rnorm(p, mean2), simplify = "array")
-test.class1 = replicate(n_test/2, rnorm(p, mean1), simplify = "array")
-test.class2 = replicate(n_test/2, rnorm(p, mean2), simplify = "array")
+train.class1 = replicate(n_train/2, rnorm(p, mean1))
+train.class2 = replicate(n_train/2, rnorm(p, mean2))
+test.class1 = replicate(n_test/2, rnorm(p, mean1))
+test.class2 = replicate(n_test/2, rnorm(p, mean2))
 
 # Generate test and training data
 train = data.frame(x = t(cbind(train.class1, train.class2)),
@@ -68,8 +69,6 @@ print(mean(rpelda.class != test$y))
 
 #######################################
 
-library(caret)
-
 # Run random forest
 
 tuneGrid <- data.frame(.mtry=(2:10))
@@ -88,8 +87,6 @@ print(mean(rf.class != test$y))
 
 #######################################
 
-library(caret)
-
 # Run partial least squares
 
 tuneGrid <- data.frame(.ncomp=1:5)
@@ -105,3 +102,21 @@ pls.class = predict(pls.model, newdata = test)
 # Calculate error
 print(mean(pls.class != test$y))
 
+
+#######################################
+
+# Run linear SVM
+
+tuneGrid <- data.frame(.C = c(0.005, 0.01, 0.05, 0.1))
+
+svm.model <- train(y ~ .,
+		data=train,
+		method='svmLinear',
+		preProcess = c("center", "scale"),
+		tuneGrid = tuneGrid,
+		trControl=trainControl(verboseIter=T))
+
+svm.class = predict(svm.model, newdata = test)
+
+# Calculate error
+print(mean(svm.class != test$y))
