4a18cc83f58aa1fc687e18deef30190e02718090
Ben <bendotli@gmail.com>
2015-07-04 21:32:33 +0100

    Add d=2 versions of RPE-KNN

--- rpecompare/rpeknn.R
+++ rpecompare/rpeknn.R
@@ -56,3 +56,60 @@ compare.axis.rpe.knn = function(data) {
 	# Calculate error
 	return(mean(rpelda.class != test$y))
 }
+
+compare.haar.rpe.knn2 = function(data) {
+	train = data$train
+	test = data$test
+	n_train = length(train$y)
+	n_test = length(test$y)
+	p = ncol(train)-1
+	d = 2 ###############################
+	
+	# Run RPE LDA with Haar measure-drawn random projections
+	rpelda.out = RPParallel(XTrain = data.matrix(train[-(p+1)]), YTrain = train$y,
+			XTest = data.matrix(test[-(p+1)]), d = d, B1 = 100, B2 = 100,
+			base = "knn", estmethod = "loo", k = 1:5,
+			cores = 1)
+	
+	# Estimate the class 1 prior probability
+	rpelda.p_hat = sum(train$y == 1)/n_train
+	
+	# Choose the best value for alpha (the voting threshold) emperically
+	rpelda.alpha_hat = RPalpha(RP.out = rpelda.out, Y = train$y, p1 = rpelda.p_hat)
+	
+	# Ensemble the base classifiers
+	rpelda.class = RPEnsembleClass(RP.out = rpelda.out, n = n_train, n.test = n_test,
+			p1 = rpelda.p_hat, alpha = rpelda.alpha_hat)
+	
+	# Calculate error
+	return(mean(rpelda.class != test$y))
+}
+
+compare.axis.rpe.knn2 = function(data) {
+	train = data$train
+	test = data$test
+	n_train = length(train$y)
+	n_test = length(test$y)
+	p = ncol(train)-1
+	d = 2 ###############################
+	
+	# Run RPE KNN with axis-aligned random projections
+	rpelda.out = RPParallel(XTrain = data.matrix(train[-(p+1)]), YTrain = train$y,
+			XTest = data.matrix(test[-(p+1)]), d = d, B1 = 100, B2 = 100,
+			base = "knn", estmethod = "loo", k = 1:5,
+			projmethod = "axis",
+			cores = 1)
+	
+	# Estimate the class 1 prior probability
+	rpelda.p_hat = sum(train$y == 1)/n_train
+	
+	# Choose the best value for alpha (the voting threshold) emperically
+	rpelda.alpha_hat = RPalpha(RP.out = rpelda.out, Y = train$y, p1 = rpelda.p_hat)
+	
+	# Ensemble the base classifiers
+	rpelda.class = RPEnsembleClass(RP.out = rpelda.out, n = n_train, n.test = n_test,
+			p1 = rpelda.p_hat, alpha = rpelda.alpha_hat)
+	
+	# Calculate error
+	return(mean(rpelda.class != test$y))
+}
