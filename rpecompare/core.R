758e42caeea2993b2d1a2ea90f4cfce3b18a0ccd
Ben <bendotli@gmail.com>
2015-07-06 23:01:00 +0100

    Refactoring into core.R

---
+++ rpecompare/core.R
@@ -0,0 +1,60 @@
+# TODO: Add comment
+# 
+# Author: Ben
+###############################################################################
+
+source("rpecompare/ldaqda.R")
+source("rpecompare/rpelda.R")
+source("rpecompare/caret.R")
+source("rpecompare/basicmodel.R")
+
+# New version that doesn't deal with lists & optimized for feeding into sapply
+misclassification.rates = function(params) {
+	model = params$model
+	classifier = params$classifier
+	r = 100
+	
+	# Load model & classifier definitions
+	source("rpecompare/all_models_and_classifiers.R")
+	
+	# Make r datasets
+	data = replicate(r, model(), simplify=FALSE)
+	
+	# Run classifier on each dataset
+	runs = sapply(data, classifier)
+	
+	return(list(misclass=mean(runs), misclass.sd=sd(runs)))
+	
+}
+
+# Input:
+#	models - list (preferably with labels!) of models
+#	classifiers - list (preferably with labels!) of classifiers
+# Output:
+# 	out$misclass - with rows as models, cols as classifiers, using labels
+# 	out$misclass.sd - with same
+rpecompare = function(models, classifiers, cores=36) {
+	# Initialize parallel processing library
+	require(snow) || install.packages("snow")
+	cl = makeCluster(cores)
+	
+	# Expand out all combinations of model+classifier as a list of lists
+	runs = expand.grid(model = models, classifier = classifiers)
+	runs.list = apply(runs, 1, as.list)
+	
+	# Run misclassification.rates on each combination
+	out = parSapply(cl, runs.list, misclassification.rates)
+	raw.misclass = out[1,]
+	raw.misclass.sd = out[2,]
+	
+	# Format into grid
+	misclass = matrix(raw.misclass, nrow=length(models),
+			dimnames=list(names(models), names(classifiers)))
+	misclass.sd = matrix(raw.misclass.sd, nrow=length(models),
+			dimnames=list(names(models), names(classifiers)))
+	
+	# Stop cluster
+	stopCluster(cl)
+	
+	return(list(misclass=misclass, misclass.sd=misclass.sd))
+}
--- rpecompare/rpecompare_model_0.R
+++ rpecompare/rpecompare_model_0.R
@@ -3,35 +3,23 @@
 # Author: Ben
 ###############################################################################
 
-# Initialize parallel processing library
-require(snow) || install.packages("snow")
-cl = makeCluster(36)
-
 # Define our models & classifiers
 source("rpecompare/all_models_and_classifiers.R")
 
-models = list(
-			function() basic.model(n_train = 50),
-			function() basic.model(n_train = 100),
-			function() basic.model(n_train = 200)
-		)
+models = list("50" = function() basic.model(n_train = 50),
+			"100" = function() basic.model(n_train = 100),
+			"200" = function() basic.model(n_train = 200))
 
 classifiers = list(
-			compare.haar.rpe.lda2,
-			compare.haar.rpe.lda,
-			compare.lda,
-			compare.haar.rpe.qda2,
-			compare.haar.rpe.qda,
-			compare.qda,
-			compare.haar.rpe.knn2,
-			compare.haar.rpe.knn
-		)
+			"RPE-H(d=2) LDA" = compare.haar.rpe.lda2,
+			"RPE-H(d=5) LDA" = compare.haar.rpe.lda,
+			"LDA" = compare.lda,
+			"RPE-H(d=2) QDA" = compare.haar.rpe.qda2,
+			"RPE-H(d=5) QDA" = compare.haar.rpe.qda,
+			"QDA" = compare.qda,
+			"RPE-H(d=2) KNN" = compare.haar.rpe.knn2,
+			"RPE-H(d=5) KNN" = compare.haar.rpe.knn)
 
-runs = expand.grid(model = models, classifier = classifiers)
-runs.list = apply(runs, 1, as.list)
 
-source("rpecompare/evaluate_misclassification_rates.R")
-out = parSapply(cl, runs.list, misclassification.rates)
-out = as.matrix(out, ncol=length(classifiers))
-
-stopCluster(cl)
+source("rpecompare/core.R")
+rpecompare(models, classifiers)
