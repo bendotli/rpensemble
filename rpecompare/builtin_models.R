ca510849eb063c363244c2bf4eee314f1a31d18b
Ben <bendotli@gmail.com>
2015-07-07 17:21:40 +0100

    Sorry, model 5 wasn't actually in RPModel, had to code it up myself.

--- rpecompare/builtin_models.R
+++ rpecompare/builtin_models.R
@@ -47,12 +47,11 @@ model.4 = function(p = 50, n_train = 50, n_test = 100, pi = 0.5) {
 	return(list(train = train, test = test))
 }
 
-model.5 = function(p = 50, n_train = 50, n_test = 100, pi = 0.5) {  
-	train.data = RPModel(5, n_train, p, pi)
-	train = data.frame(x = train.data$x, y = factor(train.data$y))
-	
-	test.data = RPModel(5, n_test, p, pi)
-	test = data.frame(x = test.data$x, y = factor(test.data$y))
+model.5 = function(n_train = 50) { 
+	data("Ozone")
+	indicators = sample(1:512, size = n_train)
+	train = data.frame(x = Ozone$x[indicators,], y = Ozone$y[indicators])
+	test = data.frame(x = Ozone$x[-indicators,], y = Ozone$y[-indicators])
 	
 	return(list(train = train, test = test))
 }
