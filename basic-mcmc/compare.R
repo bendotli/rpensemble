1b5cec6866ae13283ef77c3f715f012e056a4417
Ben <bendotli@gmail.com>
2015-08-20 09:40:53 -0700

    "Summarize" over all models rather than "compare" just on basic.model.

--- basic-mcmc/compare.R
+++ basic-mcmc/compare.R
@@ -7,7 +7,7 @@ require(parallel) || install.packages("parallel")
 source("basic-mcmc/core.R")
 
 # Compare performance of 1 projection, no optimization
-compare.no.optimization = function(R = 100) {
+summarize.no.optimization = function(R = 100) {
 	cl = makeCluster(detectCores() - 1)
 	clusterEvalQ(cl, source("basic-mcmc/core.R"))
 	
@@ -19,12 +19,12 @@ compare.no.optimization = function(R = 100) {
 	
 	stopCluster(cl)
 	
-	cat(paste0("No optimization\nMean: ", mean(results),
+	cat(paste0("Single-projection, no optimization\nMean: ", mean(results),
 		" pm ", sd(results)/sqrt(R), "\n\n"))
 }
 
 # Compare performance of 1 projection, filtered out of B2
-compare.filter = function(R = 100, B2 = 100) {
+summarize.bruteforce = function(R = 100, B2 = 100) {
 	cl = makeCluster(detectCores() - 1)
 	clusterEvalQ(cl, source("basic-mcmc/core.R"))
 
@@ -40,13 +40,13 @@ compare.filter = function(R = 100, B2 = 100) {
 
 	stopCluster(cl)
 
-	cat(paste0("Filtering optimization\nMean: ", mean(results),
+	cat(paste0("Single-projection, bruteforce optimization\nMean: ", mean(results),
 			" pm ", sd(results)/sqrt(R), "\n\n"))
 }
 
 
 # Compare performance of 1 projection, MCMC optimization
-compare.mcmc = function(R = 100, B2 = 100) {
+summarize.mcmc = function(R = 100, B2 = 100) {
 	cl = makeCluster(detectCores() - 1)
 	clusterEvalQ(cl, source("basic-mcmc/core.R"))
 	
@@ -58,7 +58,7 @@ compare.mcmc = function(R = 100, B2 = 100) {
 	
 	stopCluster(cl)
 	
-	cat(paste0("MCMC\nMean: ", mean(results),
+	cat(paste0("Single-projection, MCMC posterior predictive sampling\nMean: ", mean(results),
 		" pm ", sd(results)/sqrt(R), "\n\n"))
 }
 
@@ -73,52 +73,62 @@ class.labels.to.strings = function(label) {
 
 
 # Compare performance of B1 projections, MCMC optimization
-compare.ensemble.mcmc = function(R = 100, B1 = 100, B2 = 100) {
+summarize.ensemble.mcmc = function(R = 100, B1 = 100, B2 = 100) {
 	cl = makeCluster(detectCores() - 1)
 	clusterEvalQ(cl, source("basic-mcmc/core.R"))
 	clusterExport(cl, "class.labels.to.integers")
 	clusterExport(cl, "class.labels.to.strings")
 	
-	results = parSapply(cl, 1:R, function(i) {
-				data = basic.model()
+	cat("Ensemble, MCMC posterior predictive sampling\n")
+	
+	info = function(modelnum) {
+		results = parSapply(1:R, function(i) {
+					data = match.fun(paste0("model.", modelnum))()
+					
+					# Get B1 (independently) optimized projections over training set
+					mats = lapply(1:B1, function(...) mcmc_optimize(data$train, B2 = B2))
+					
+					# Fit B1 LDA models
+					models = lapply(mats, function(mat) {
+							return(lda(y ~ ., data = project(data$train, mat)))
+						})
 				
-				# Get B1 (independently) optimized projections over training set
-				mats = lapply(1:B1, function(...) mcmc_optimize(data$train, B2 = B2))
-				
-				# Fit B1 LDA models
-				models = lapply(mats, function(mat) {
-						return(lda(y ~ ., data = project(data$train, mat)))
-					})
-			
-				# Obtain B1 class predictions on training set
-				# (we transform the class labels into integers {1, 2} for use
-				# with RPalpha)
-				tr.pred = mapply(function(model, mat) {
-						class = predict(model)$class
-						return(sapply(class, class.labels.to.integers))
-					}, models, mats)
-				
-				# Compute estimate of alpha (well, alpha+1)
-				Y = sapply(data$train$y, class.labels.to.integers)
-				alpha.hat = RPalpha(tr.pred, Y, 2 - mean(Y))
-				
-				# Obtain B1 class predictions on test set
-				# (again, transforming class labels into integers)
-				te.pred = mapply(function(model, mat) {
-						class = predict(model, newdata = project(data$test, mat))$class
-						return(sapply(class, class.labels.to.integers))
-					}, models, mats)
-				
-				# Consolidate into ensemble predictions using alpha
-				vote = 1 + as.numeric(rowMeans(te.pred) > alpha.hat)
-				out = sapply(vote, class.labels.to.strings)
-				
-				# Return misclassification rate on test set
-				return(out != data$test$y)
-			})
+					# Obtain B1 class predictions on training set
+					# (we transform the class labels into integers {1, 2} for use
+					# with RPalpha)
+					tr.pred = mapply(function(model, mat) {
+							class = predict(model)$class
+							return(sapply(class, class.labels.to.integers))
+						}, models, mats)
+					
+					# Compute estimate of alpha (well, alpha+1)
+					Y = sapply(data$train$y, class.labels.to.integers)
+					alpha.hat = RPalpha(tr.pred, Y, 2 - mean(Y))
+					
+					# Obtain B1 class predictions on test set
+					# (again, transforming class labels into integers)
+					te.pred = mapply(function(model, mat) {
+							class = predict(model, newdata = project(data$test, mat))$class
+							return(sapply(class, class.labels.to.integers))
+						}, models, mats)
+					
+					# Consolidate into ensemble predictions using alpha
+					vote = 1 + as.numeric(rowMeans(te.pred) > alpha.hat)
+					out = sapply(vote, class.labels.to.strings)
+					
+					# Return misclassification rate on test set
+					return(out != data$test$y)
+				})
+		cat(paste0("Model ", modelnum, ": ", mean(results),
+						" pm ", sd(results)/sqrt(R), "\n"))
+	}
+	lapply(1:5, info)
+	
+	cat("\n")
 	
 	stopCluster(cl)
+}
+
+summarize.ensemble.bruteforce = function(R = 100, B1 = 100, B2 = 100) {
 	
-	cat(paste0("Ensemble MCMC\nMean: ", mean(results),
-					" pm ", sd(results)/sqrt(R), "\n\n"))
 }
